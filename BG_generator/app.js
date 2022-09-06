3d6b5e335fd900b9ebf1d614aad81d7f61ebcade
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-09-06 13:58:43 +0300

    BG gradient
    finished

--- BG_generator/app.js
+++ BG_generator/app.js
@@ -4,16 +4,8 @@ let body = document.querySelector("body");
 let direction = document.querySelector('input[name="direction"]');
 let result = document.querySelector(".CSSoutput");
 
-
-
-function currentSettings() {
-    var CSSprop = window.getComputedStyle(body,null).getPropertyValue("background-image")
-    console.log(CSSprop)
-    result.textContent = CSSprop
-}
-
-function StyleSelection() {
- let directionValue = document.querySelector('input[name="direction"]:checked').value;
+function StyleSelection(){
+let directionValue = document.querySelector('input[name="direction"]:checked').value;
   body.style.background =
     "linear-gradient(" +
     directionValue +
@@ -24,8 +16,12 @@ function StyleSelection() {
     ")";
     console.log(directionValue)
     currentSettings()
+} 
+function currentSettings() {
+    var css_string = window.getComputedStyle(body,null).getPropertyValue("background-image")
+    result.textContent = css_string;
 }
 
-direction.addEventListener("change",StyleSelection())
+body.addEventListener("change",StyleSelection)
 color1.addEventListener("input",StyleSelection)
 color2.addEventListener("input",StyleSelection)
