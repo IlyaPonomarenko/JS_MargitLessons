c4f7e0898d84d86b714444f1d78ba947213b2036
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-09-21 14:48:01 +0300

    Updated

--- Speed_game/app.js
+++ Speed_game/app.js
@@ -2,35 +2,59 @@ const startBtn = document.querySelector(".startBtn");
 const stopBtn = document.querySelector(".endBtn");
 const circles = document.querySelectorAll(".circle");
 const score = document.querySelector(".score");
+const overlay = document.querySelector(".overlay")
+const refresh = document.querySelector(".btn")
 let scoreCount = 0;
+let active = 0;
+let timer;
 
+const getRndInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 const gameStart = () => {
-  randomHighlight();
+  circles.forEach((circle) => {
+    circle.classList.remove("active");
+  });
+  let nextActive = newNum(active);
+  active = nextActive;
+  console.log(active);
+  timer = setTimeout(gameStart, 1000);
+  function newNum(active) {
+    let nextActive = getRndInt(0, 3);
+    if (nextActive != active) {
+      return nextActive;
+    } else {
+      return newNum(active);
+    }
+  }
+  circles[nextActive].classList.add("active");
 };
 const gameEnd = () => {
   circles.forEach((circle) => {
     circle.classList.remove("active");
   });
-};
-const clickDiv = (e) => {
-  console.log("clicked div", e);
-  scoreCount++;
-  score.innerText = scoreCount;
-  randomHighlight();
-};
-const randomHighlight = () => {
-    let randDiv = Math.floor(Math.random() * 4);
-    setTimeout(()=>{
-        circles.forEach((circle) => {
-            circle.classList.remove("active");
-          });
-          console.log(randDiv);
-          circles[randDiv].classList.add("active");
-    },1000)
+  score.textContent = 0;
+  clearTimeout(timer);
+  overlay.style.visibility = "visible"
 };
 
+const clickDiv = (e) => {
+  console.log("clicked div", e);
+  if (circles[e].classList.contains("active")== true) {
+    scoreCount++;
+    score.textContent = scoreCount;
+  } else {
+    gameEnd();
+  }
+};
 circles.forEach((circle, e) => {
   circle.addEventListener("click", () => clickDiv(e));
 });
+const refreshPage = () =>{
+    window.location.reload()
+}
+
+
 startBtn.addEventListener("click", gameStart);
 stopBtn.addEventListener("click", gameEnd);
+refresh.addEventListener("click",refreshPage)
