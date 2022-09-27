71732fc81a5d0e7c744621c4db16dd69663dcda6
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-09-27 12:51:03 +0300

    added sounds

--- Speed_game/app.js
+++ Speed_game/app.js
@@ -4,16 +4,36 @@ const circles = document.querySelectorAll(".circle");
 const score = document.querySelector(".score");
 const overlay = document.querySelector(".overlay");
 const refresh = document.querySelector(".btn");
+const infoButton = document.querySelector(".infoBtn")
+const finalResult = document.querySelector("#result")
+const startAudio = new Audio('/sounds/mixkit-cool-interface-click-tone-2568.wav')
+const endAudio = new Audio('/sounds/mixkit-negative-tone-interface-tap-2569.wav')
+const infoAudio = new Audio('/sounds/lembas-elvish-waybread-one-small-bite-is-enough-to-fill-the-stomach-of-a-grown-man.wav')
 let scoreCount = 0;
 let active = 0;
 let timer;
 let speed = 1000;
 let rounds = 0;
 
+const playInfoAudio = () => {
+  infoAudio.play();
+}
+const playAudioClick = () =>{
+  startAudio.play();
+}
+const playAudioEnd = () => {
+  endAudio.play()
+}
+
 const getRndInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 const gameStart = () => {
+  
+  circles.forEach((circle) => {
+    circle.style.pointerEvents="auto";
+  });
+  
   if (rounds >= 1) {
     gameEnd();
   } else {
@@ -23,7 +43,6 @@ const gameStart = () => {
 
     let nextActive = newNum(active);
     active = nextActive;
-    console.log(active);
     timer = setTimeout(gameStart, speed);
     speed = speed - 50;
 
@@ -38,19 +57,29 @@ const gameStart = () => {
     circles[nextActive].classList.add("active");
     rounds++;
   }
+  
 };
 const gameEnd = () => {
+  playAudioEnd();
   circles.forEach((circle) => {
     circle.classList.remove("active");
   });
+
   score.textContent = 0;
   clearTimeout(timer);
+
+  if (scoreCount < 5){
+    finalResult.textContent = `Your final score is ${scoreCount}. Are you even trying?`
+  } else {
+    finalResult.textContent = `Your final score is ${scoreCount}. Give it another try!`
+  }
+  
   overlay.style.visibility = "visible";
 };
 
 const clickDiv = (e) => {
-  console.log("clicked div", e);
   if (circles[e].classList.contains("active") == true) {
+    playAudioClick();
     scoreCount++;
     rounds--;
     score.textContent = scoreCount;
@@ -68,3 +97,4 @@ const refreshPage = () => {
 startBtn.addEventListener("click", gameStart);
 stopBtn.addEventListener("click", gameEnd);
 refresh.addEventListener("click", refreshPage);
+infoButton.addEventListener("click", playInfoAudio)
