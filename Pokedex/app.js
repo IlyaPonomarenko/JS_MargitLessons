16165e7808cbad6258f132b0ecf47ae8dd1e2ee2
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-09-28 14:11:24 +0300

    updated icons

--- Pokedex/app.js
+++ Pokedex/app.js
@@ -1,5 +1,6 @@
 const row = document.querySelector(".row-1")
-const num_of_fetches = 40;
+const btn_gen2 = document.querySelector(".gen1")
+const num_of_fetches = 300;
 
     const fetchMultiplePokes = async () =>{
         for (let i=1; i <= num_of_fetches; i++){
@@ -16,11 +17,11 @@ const num_of_fetches = 40;
     const createCard = (data) => {
         const pokemonCard = document.createElement("div");
         pokemonCard.classList.add("card");
-        const {id, name, sprites, types} = data;
+        const {name, sprites, types} = data;
         const type = types[0].type.name
         const pokemonInnerHtml =  ` 
           <div class="card-icon">
-            <img src="${sprites.front_default}" alt="${name}">
+            <img src="${sprites.other.home.front_default}" alt="${name}">
           </div>
          <div class="card-text">
             <h2>${name}</h2>
