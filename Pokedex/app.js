c9d322e1db52c54ce3f29a95a78e223860cf59c5
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-09-28 12:46:05 +0300

    update

--- Pokedex/app.js
+++ Pokedex/app.js
@@ -1,5 +1,5 @@
-const container = document.querySelector("#poke-container")
-const num_of_fetches = 100;
+const row = document.querySelector(".row-1")
+const num_of_fetches = 40;
 
     const fetchMultiplePokes = async () =>{
         for (let i=1; i <= num_of_fetches; i++){
@@ -10,7 +10,24 @@ const num_of_fetches = 100;
         const url = `https://pokeapi.co/api/v2/pokemon/${id}`
         const responce = await fetch(url)
         const data = await responce.json();
-        console.log(data)
+        createCard(data)
     }
     
-    fetchMultiplePokes();
+    const createCard = (data) => {
+        const pokemonCard = document.createElement("div");
+        pokemonCard.classList.add("card");
+        const {id, name, sprites, types} = data;
+        const type = types[0].type.name
+        const pokemonInnerHtml =  ` 
+          <div class="card-icon">
+            <img src="${sprites.front_default}" alt="${name}">
+          </div>
+         <div class="card-text">
+            <h2>${name}</h2>
+            <p>Type:${type}</p>
+         </div>
+    `;
+    pokemonCard.innerHTML=pokemonInnerHtml;
+        row.appendChild(pokemonCard);
+    }
+    fetchMultiplePokes()
