6eb51e320da5ee9801b8756fb6e79937c5c1b2f5
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-10-02 13:36:51 +0300

    new task

---
+++ SantoshTask_ToDoList/script.js
@@ -0,0 +1,12 @@
+const todoForm = document.querySelector("#todoForm");
+const taskNameInput = todoForm['task']
+const taskDescInput = todoForm['desc']
+const tasksCont = document.querySelector(".tasks")
+
+const taskArr = []
+
+const addAtask = (task, desc) => {
+
+}
+
+const addTask = () =>
